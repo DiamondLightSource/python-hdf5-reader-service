a5be47de97e01b84755e7aff76b3626e00d7fd9f
OCopping <OCopping@users.noreply.github.com>
2022-03-22 14:55:43 +0000

    deploy: b8ec8fd96a8ef4050d1d5ab59df24a694156f75c

--- main/_static/documentation_options.js
+++ main/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1+22.ge15f006',
+    VERSION: '0.1.1+24.gb8ec8fd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
