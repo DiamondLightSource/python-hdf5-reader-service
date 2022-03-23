d0581c21c8f2f3a47ce91134f0d7fde0e20beb88
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-03-23 13:49:28 +0000

    Add CLI

--- src/hdf5_reader_service/main.py
+++ src/hdf5_reader_service/main.py
@@ -1,7 +1,14 @@
 import os
 
+import click
 from fastapi import FastAPI  # , Response, status
 
+# @app.get("/busy")
+# def busy(response: Response):
+#     if LOCK.locked():
+#         response.status_code = status.HTTP_423_LOCKED
+#     return {"busy": LOCK.locked()}
+from ._version_git import __version__
 from .blueprints import info, search, shapes, slice, tree
 
 # Setup the app
@@ -19,8 +26,23 @@ def index():
     return {"INFO": "Please provide a path to the HDF5 file, e.g. '/file/<path>'."}
 
 
-# @app.get("/busy")
-# def busy(response: Response):
-#     if LOCK.locked():
-#         response.status_code = status.HTTP_423_LOCKED
-#     return {"busy": LOCK.locked()}
+@click.group(invoke_without_command=True)
+@click.option(
+    "-h",
+    "--host",
+    type=str,
+    help="host IP",
+    default="0.0.0.0",
+)
+@click.option(
+    "-p",
+    "--port",
+    type=int,
+    help="host port",
+    default="8000",
+)
+@click.version_option(version=__version__)
+def main(host: str, port: int) -> None:
+    import uvicorn
+
+    uvicorn.run(app, host=host, port=port)
