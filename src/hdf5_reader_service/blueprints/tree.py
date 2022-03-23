9281ddc570ccb6c6a654adebd6c18c8faa02b9c2
Callum Forrester <callum.forrester@diamond.ac.uk>
2022-03-23 08:46:40 +0000

    WIP on integration with tree endpoint

--- src/hdf5_reader_service/blueprints/tree.py
+++ src/hdf5_reader_service/blueprints/tree.py
@@ -1,3 +1,4 @@
+import imp
 import multiprocessing as mp
 import os
 import time
@@ -9,7 +10,7 @@ from fastapi import APIRouter
 from starlette.responses import JSONResponse
 
 from hdf5_reader_service.blueprints.info import metadata
-from hdf5_reader_service.utils import NumpySafeJSONResponse
+from hdf5_reader_service.utils import NumpySafeJSONResponse, h5_tree_map
 
 SWMR_DEFAULT = bool(int(os.getenv("HDF5_SWMR_DEFAULT", "1")))
 
@@ -36,22 +37,22 @@ def fetch_nodes(path: str, subpath: str, queue: mp.Queue) -> None:
 
     path = "/" + path
 
-    tr: Dict[str, Any] = defaultdict(dict)
+    # tr: Dict[str, Any] = defaultdict(dict)
 
-    def visit_node(
-        addr: Union[str, List[str]], node: h5py.HLObject, tree: Dict[str, Any] = tr
-    ) -> None:
-        if isinstance(addr, str):
-            return visit_node(addr.split("/"), node)
-        elif len(addr) > 1:
-            return visit_node(addr[1:], node, tree[addr[0]]["subnodes"])
-        else:
-            tree[addr[0]] = {
-                "subnodes": defaultdict(dict),
-                "metadata": metadata(node),
-            }
+    # def visit_node(
+    #     addr: Union[str, List[str]], node: h5py.HLObject, tree: Dict[str, Any] = tr
+    # ) -> None:
+    #     if isinstance(addr, str):
+    #         return visit_node(addr.split("/"), node)
+    #     elif len(addr) > 1:
+    #         return visit_node(addr[1:], node, tree[addr[0]]["subnodes"])
+    #     else:
+    #         tree[addr[0]] = {
+    #             "subnodes": defaultdict(dict),
+    #             "metadata": metadata(node),
+    #         }
 
-    with h5py.File(path, "r", swmr=SWMR_DEFAULT, libver="latest") as file:
-        file.visititems(visit_node)
+    with h5py.File(path, "r", swmr=SWMR_DEFAULT, libver="latest") as f:
+        tree = h5_tree_map(lambda _, obj: metadata(obj), f)
 
-    queue.put(tr)
+    queue.put(tree)
